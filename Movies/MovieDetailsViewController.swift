9f11be87e54dbe7a08cd9e19e3a69e4eea0e1ef2
Sheroz Nazhmudinov <sheroz.nazhmudinov@bueller.us>
2018-02-22 13:26:35 +0100

    movies-ios - show the movie poster in details screen

--- Movies/MovieDetailsViewController.swift
+++ Movies/MovieDetailsViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 import SnapKit
+import Kingfisher
 
 class MovieDetailsViewController: UIViewController {
 
+    static let kWidthToHeightRatio = CGFloat(1.79)
+    
     var movie: Result?
+    lazy var posterImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width / MovieDetailsViewController.kWidthToHeightRatio))
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if movie != nil {
+            setupUI()
+        }
+    }
+    
+    func setupUI() {
+        //Movie poster
+        self.view.addSubview(posterImageView)
+        let imageUrl = URL(string: MovieListViewController.kBasePosterUrl + (movie?.backdropPath)!)
+        posterImageView.kf.setImage(with: imageUrl)
+        
+        //Movie title
+        
     }
 }
