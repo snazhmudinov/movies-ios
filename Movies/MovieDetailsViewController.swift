f49a4cdb863d248ed70ca22485b033a34daf5b16
snazhmudinov <snazhmudinov@gmail.com>
2018-02-25 10:09:07 +0100

    title and description sections

--- Movies/MovieDetailsViewController.swift
+++ Movies/MovieDetailsViewController.swift
@@ -12,10 +12,18 @@ import Kingfisher
 
 class MovieDetailsViewController: UIViewController {
 
-    static let kWidthToHeightRatio = CGFloat(1.79)
-    
+    @IBOutlet weak var scrollView: UIScrollView!
     var movie: Result?
+    
+    //Static constants
+    static let kWidthToHeightRatio = CGFloat(1.79)
+    static let kContentOffset = 8
+    
+    //UI elements
     lazy var posterImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width / MovieDetailsViewController.kWidthToHeightRatio))
+    lazy var movieTitle = UILabel()
+    lazy var movieDescription = UILabel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +35,51 @@ class MovieDetailsViewController: UIViewController {
     
     func setupUI() {
         //Movie poster
-        self.view.addSubview(posterImageView)
+        scrollView.addSubview(posterImageView)
         let imageUrl = URL(string: MovieListViewController.kBasePosterUrl + (movie?.backdropPath)!)
         posterImageView.kf.setImage(with: imageUrl)
         
         //Movie title
+        let titleLabel = UILabel()
+        titleLabel.text = "Title".uppercased()
+        titleLabel.textColor = UIColor.gray
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        scrollView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) -> Void in
+           make.top.equalTo(posterImageView.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
+           make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
+        }
         
+        movieTitle.text = movie?.originalTitle
+        scrollView.addSubview(movieTitle)
+        movieTitle.numberOfLines = 0
+        movieTitle.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(titleLabel.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
+            make.right.equalTo(view.snp.right).inset(MovieDetailsViewController.kContentOffset)
+            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
+        }
+        
+        //Movie description
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Description".uppercased()
+        descriptionLabel.textColor = UIColor.gray
+        descriptionLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        scrollView.addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(movieTitle.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
+            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
+        }
+        
+        movieDescription.text = movie?.overview
+        scrollView.addSubview(movieDescription)
+        movieDescription.numberOfLines = 0
+        movieDescription.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
+            make.right.equalTo(view.snp.right).inset(MovieDetailsViewController.kContentOffset)
+            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
+            
+        }
+        
+        scrollView.contentSize = view.frame.size
     }
 }
