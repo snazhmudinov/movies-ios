8f8e9e843ae975f65db59b65e5356b2f3ec3a94d
snazhmudinov <snazhmudinov@gmail.com>
2018-03-04 11:31:07 +0100

    fixed the scrolling

--- Movies/MovieDetailsViewController.swift
+++ Movies/MovieDetailsViewController.swift
@@ -14,6 +14,11 @@ import Alamofire
 class MovieDetailsViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescriptionTextView: UILabel!
+    
+    
     var movie: Movie.Result?
     var trailers: Trailer?
     var youtubeId: String?
@@ -23,81 +28,31 @@ class MovieDetailsViewController: UIViewController {
     static let kContentOffset = 8
     static let kYouTubeBaseUrl = "https://www.youtube.com/watch?v="
     
-    //UI elements
-    lazy var posterImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width / MovieDetailsViewController.kWidthToHeightRatio))
-    lazy var movieTitle = UILabel()
-    lazy var movieDescription = UILabel()
-    lazy var watchTrailerButton = UIButton(type: UIButtonType.system)
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if movie != nil {
-            setupUI()
-        }
-    }
-    
-    func setupUI() {
-        //Get YoutTube trailer link
-        getYouTubeLink()
-        
-        //Movie poster
-        scrollView.addSubview(posterImageView)
-        let imageUrl = URL(string: MovieListViewController.kBasePosterUrl + (movie?.backdropPath)!)
-        posterImageView.kf.setImage(with: imageUrl)
-        
-        //Movie title
-        let titleLabel = UILabel()
-        titleLabel.text = "Title".uppercased()
-        titleLabel.textColor = UIColor.gray
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 12)
-        scrollView.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { (make) -> Void in
-           make.top.equalTo(posterImageView.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
-           make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
-        }
-        
-        movieTitle.text = movie?.originalTitle
-        scrollView.addSubview(movieTitle)
-        movieTitle.numberOfLines = 0
-        movieTitle.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(titleLabel.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
-            make.right.equalTo(view.snp.right).inset(MovieDetailsViewController.kContentOffset)
-            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
-        }
-        
-        //Movie description
-        let descriptionLabel = UILabel()
-        descriptionLabel.text = "Description".uppercased()
-        descriptionLabel.textColor = UIColor.gray
-        descriptionLabel.font = UIFont.boldSystemFont(ofSize: 12)
-        scrollView.addSubview(descriptionLabel)
-        descriptionLabel.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(movieTitle.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
-            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
-        }
-        
-        movieDescription.text = movie?.overview
-        scrollView.addSubview(movieDescription)
-        movieDescription.numberOfLines = 0
-        movieDescription.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(descriptionLabel.snp.bottom).offset(MovieDetailsViewController.kContentOffset)
-            make.right.equalTo(view.snp.right).inset(MovieDetailsViewController.kContentOffset)
-            make.left.equalTo(view.snp.left).offset(MovieDetailsViewController.kContentOffset)
+            //Get YoutTube trailer link
+            getYouTubeLink()
             
+            //Movie poster
+            let imageUrl = URL(string: MovieListViewController.kBasePosterUrl + (movie?.backdropPath)!)
+            posterImageView.kf.setImage(with: imageUrl)
+            let posterHeight = UIScreen.main.bounds.width / MovieDetailsViewController.kWidthToHeightRatio
+            let newFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: posterHeight)
+            posterImageView.frame = newFrame
+            posterImageView.snp.makeConstraints { (make) -> Void in
+                make.height.equalTo(posterHeight)
+            }
+            
+            //Movie title
+            movieTitleLabel.text = movie?.originalTitle
+            //Movie description
+            movieDescriptionTextView.text = movie?.overview
+            movieDescriptionTextView.sizeToFit()
+            
+            scrollView.adjustToFitContent()
         }
-        
-        scrollView.addSubview(watchTrailerButton)
-        watchTrailerButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
-        watchTrailerButton.tintColor = UIColor.white
-        watchTrailerButton.addTarget(self, action: #selector(MovieDetailsViewController.playTrailer), for: UIControlEvents.touchUpInside)
-        watchTrailerButton.snp.makeConstraints { (make) -> Void in
-            make.centerX.equalTo(posterImageView.snp.centerX)
-            make.centerY.equalTo(posterImageView.snp.centerY)
-        }
-        
-        scrollView.contentSize = view.frame.size
     }
     
     func getYouTubeLink() {
@@ -106,33 +61,43 @@ class MovieDetailsViewController: UIViewController {
         Alamofire.request(youtTubeLink, method: .get, parameters: MovieListViewController.kParameters, encoding: URLEncoding.queryString, headers: nil).responseObject { (response: DataResponse<Trailer>) in
             if response.result.isSuccess {
                 self.trailers = response.result.value
-                self.youtubeId = self.trailers?.results?[0].key
+                print("Number of trailers: \(self.trailers?.results?.count ?? 0)")
             }
         }
     }
     
-    @objc func playTrailer() {
-        guard youtubeId != nil else {
-            let alert = UIAlertController(title: "Error", message: "No trailer available", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default) { handler in
-                alert.dismiss(animated: true, completion: nil)
-            }
-            alert.addAction(okAction)
-            self.present(alert, animated: true, completion: nil)
-            return
+//    @objc func playTrailer() {
+//        guard youtubeId != nil else {
+//            let alert = UIAlertController(title: "Error", message: "No trailer available", preferredStyle: .alert)
+//            let okAction = UIAlertAction(title: "OK", style: .default) { handler in
+//                alert.dismiss(animated: true, completion: nil)
+//            }
+//            alert.addAction(okAction)
+//            self.present(alert, animated: true, completion: nil)
+//            return
+//        }
+//
+//        //Try to open in YouTube app, if not installed - open Safari
+//        let youtubeUrl = NSURL(string: "youtube://\(youtubeId!)")
+//        if UIApplication.shared.canOpenURL(youtubeUrl! as URL) {
+//            UIApplication.shared.open(youtubeUrl! as URL, options: [:], completionHandler: nil  )
+//        } else {
+//            let externalYouTubeLink = URL(string: MovieDetailsViewController.kYouTubeBaseUrl + youtubeId!)
+//
+//            guard let link = externalYouTubeLink else  { return }
+//            if UIApplication.shared.canOpenURL(link) {
+//                UIApplication.shared.open(link, options: [:], completionHandler: nil)
+//            }
+//        }
+//    }
+}
+
+extension UIScrollView {
+    func adjustToFitContent() {
+        var contentRect = CGRect.zero
+        for view in self.subviews {
+            contentRect = contentRect.union(view.frame)
         }
-        
-        //Try to open in YouTube app, if not installed - open Safari
-        let youtubeUrl = NSURL(string: "youtube://\(youtubeId!)")
-        if UIApplication.shared.canOpenURL(youtubeUrl! as URL) {
-            UIApplication.shared.open(youtubeUrl! as URL, options: [:], completionHandler: nil  )
-        } else {
-            let externalYouTubeLink = URL(string: MovieDetailsViewController.kYouTubeBaseUrl + youtubeId!)
-            
-            guard let link = externalYouTubeLink else  { return }
-            if UIApplication.shared.canOpenURL(link) {
-                UIApplication.shared.open(link, options: [:], completionHandler: nil)
-            }
-        }
+        self.contentSize = contentRect.size
     }
 }
